b73b74000713849c9584e84be2e72c5d9ea37280
Andrius <Andriusj6@hotmail.com>
2020-02-25 15:51:08 +0000

    header scss

--- src/components/.layout/Header.js
+++ src/components/.layout/Header.js
@@ -1,11 +1,8 @@
 import React from 'react'
+import './Header.scss'
 
 const Header = () => {
-  return (
-    <div>
-      <p>Header</p>
-    </div>
-  )
+  return <div className='header-container'></div>
 }
 
 export default Header
