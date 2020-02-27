1fc823d2d44b493f96ab4df7735e2bfbbee04792
Andrius <Andriusj6@hotmail.com>
2020-02-27 14:30:01 +0000

    tests and dependancies

--- src/tests/Homepage.spec.js
+++ src/tests/Homepage.spec.js
@@ -1,9 +1,6 @@
 import React from 'react'
-import { render, cleanup, fireEvent } from '@testing-library/react'
+import { render, cleanup } from '@testing-library/react'
 import Homepage from '../components/homepage/Homepage'
-import { renderHook } from '@testing-library/react-hooks'
-import fetchMock from 'fetch-mock'
-import { act } from 'react-test-renderer'
 
 afterAll(cleanup)
 
@@ -12,15 +9,4 @@ describe('<Homepage />', () => {
     const { queryByTestId } = render(<Homepage />)
     expect(queryByTestId).toBeTruthy()
   })
-
-  beforeAll(() => {
-    global.fetch = fetch
-  })
-
-  afterAll(() => {
-    fetchMock.restore()
-  })
-
-  it("should return data with a successful request", async () => {
-    const { result } = renderHook(() => useDataApi());
 })
